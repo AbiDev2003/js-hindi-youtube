2f88cde7c66dba75aafd345da5588f92a9e46bba
Abinash Dash <2003abinashdash@gmail.com>
2024-07-23 11:17:07 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all Js code as newer version
+// alert(3+3) //we are using node JS not browser
+
+console.log(3+3); //code readability should be high
+console.log("Hitesh Sir")
+let name="HItesh"
+let age=18
+let isLoggedIn=false
+// number =>2 to power
+// bigint
+// string => ""
+// bolean=>true/false
+// null => standalone value
+// undefined=>
+    // symbol=>unique 
+// object 
+console.log(typeof "hitesh"); //string
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
